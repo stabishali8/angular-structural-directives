d63a349250367c4b70ebbc537c18e0e2124b6b26
amirbasheer <amirbashir880@gmail.com>
2021-02-24 16:35:00 +0500

    Angular Tutorial - 16 - Pipes

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,30 +1,75 @@
-import { Component, EventEmitter, Input,Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template:
-  `
+    `
   <h2>
-    {{"Degrees vs Skills " + name}}
+    {{"Degrees vs Skills " + name | lowercase}}
+  </h2>
+  <h2>
+    {{"Degrees vs Skills " + name | lowercase}}
+  </h2>
+  <h2>
+    {{"Degrees vs Skills " + name | titlecase}}
+  </h2>
+  <h2>
+    {{"Degrees vs Skills " + name | slice:3:5}}
+  </h2>
+  <h2>
+    {{"name | json " + person | json}}
+  </h2>
+  <h2>
+    {{"5.678 " | number:'1.2-3'}}
+  </h2>
+  <h2>
+    {{"5.678 " | number:'1.2-3'}}
+  </h2>
+  <h2>
+    {{"5.678 " | number:'1.2-3'}}
+  </h2>
+  <h2>
+  {{"5.678 " | percent}}
+  </h2>
+  <h2>
+  {{"5.678 " | currency}}
+  </h2>
+  <h2>
+  {{"5.678 " | currency:'USD'}}
+  </h2>
+  <h2>
+  {{ date }}
+  </h2>
+  <h2>
+  {{ date | date:'short' }}
+  </h2>
+  <h2>
+  {{ date | date:'shortDate' }}
+  </h2>
+  <h2>
+  {{ date | date:'shortTime' }}
   </h2>
 
-  <button (click)="fireEvent()">
-    Send Event
-  </button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  @Input('parentData') public name:any; 
+  @Input('parentData') public name: any;
 
+  public person = {
+    "firstName" : "Syed Tabish",
+    "lastName" : "Ali",
+    "age":26
+  }
+  public date = new Date();
   @Output() public childEvent = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  fireEvent(){
+  fireEvent() {
     this.childEvent.emit('Hey Syed Tabish Ali')
   }
 
