03d1d93907954e59376304390b0e8b6d89ac039f
amirbasheer <amirbashir880@gmail.com>
2021-02-24 12:05:23 +0500

    Angular Tutorial - 13 - ngSwitch Directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,22 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template:
-  `<div *ngIf="displayName; then thenBlock; else elseBlock"></div>
-  
-  <ng-template #thenBlock>
-    <h2>Syed Tabish Ali</h2>
-  </ng-template>
-
-  <ng-template #elseBlock>
-    <h2>Degree VS Skills</h2>
-  </ng-template>
+  `
+  <div [ngSwitch]="color"> 
+    <div *ngSwitchCase="'red'">You picked red color</div>
+    <div *ngSwitchCase="'blue'">You picked blue color</div>
+    <div *ngSwitchCase="'green'">You picked green color</div>
+    <div *ngSwitchDefault>Pick again</div>
+  </div>
   
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public displayName = false; 
+  public color = "red"; 
   constructor() { }
 
   ngOnInit(): void {
